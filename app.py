82c8ae91661be6105f48a01169bcd494b783c1f7
Mousam Das <209365734+Mousam-gift@users.noreply.github.com>
2026-05-19 01:51:25 +0530

    fixing error

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
 
 for msg in st.session_state.chat_history:
-    with st.chat_history(msg["role"]):
+    with st.chat_message(msg["role"]):
         st.markdown(msg["content"])
 
 user_input = st.text_input("Ask me anything:")
